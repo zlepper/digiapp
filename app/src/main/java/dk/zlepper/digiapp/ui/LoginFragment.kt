85fa4656606c8c56cde08c570fef5b47b1337598
zlepper <hansen13579@gmail.com>
2019-05-24 17:53:05 +0200

    Automatically close the keyboard on successful login

--- app/src/main/java/dk/zlepper/digiapp/ui/LoginFragment.kt
+++ app/src/main/java/dk/zlepper/digiapp/ui/LoginFragment.kt
@@ -1,9 +1,13 @@
 package dk.zlepper.digiapp.ui
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.room.Room
@@ -57,6 +61,13 @@ class LoginFragment : Fragment() {
                 val dao = db.userCredentialsDao()
                 dao.removeStoredCredentials()
                 dao.insertCredentials(UserCredentials(username, password))
+
+                // Close the keyboard, since it doesn't autoclose when the input disappears...
+                // https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+                activity?.currentFocus?.let { v ->
+                    val imm = getSystemService<InputMethodManager>(v.context, InputMethodManager::class.java)
+                    imm?.hideSoftInputFromWindow(v.windowToken, 0)
+                }
 
                 val action = LoginFragmentDirections.actionLoginFragmentToAssetListFragment()
                 findNavController().navigate(action)
